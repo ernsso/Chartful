7392248dd472b9494dc6eb9ff4a9f6013c680d7c
mperroteau <marine.perroteau@gmail.com>
2013-07-11 21:49:16 +0200

    Classe Information et classe User

---
+++ Solution/Chartful/Model/Information.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chartful
+{
+    class Information
+    {
+        string doc_name { get; set; }
+        string content { get; set; }
+
+        public Information(string _doc_name, string _content)
+        {
+            doc_name = _doc_name;
+            content = _content;
+        }
+    }
+}
