53b29781b1a957429f207f8a45a4a8ab703a0c51
Loïc M'Bimi <loic_mb@hotmail.fr>
2013-09-09 02:54:33 +0200

    Refresh

---
+++ Solution/Chartful/Controls/MenuList.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Chartful.Model;
+using Chartful.Pages;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chartful.Controls
+{
+    /// <summary>
+    /// Logique d'interaction pour MenuList.xaml
+    /// </summary>
+    public partial class MenuList : UserControl
+    {
+        public MenuList()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ListItemsProperty = DependencyProperty.Register("ListItems",
+            typeof(ObservableCollection<Document>),
+            typeof(MenuList),
+            null);
+
+        public ObservableCollection<Document> ListItems
+        {
+            get { return (ObservableCollection<Document>)GetValue(ListItemsProperty); }
+            set { SetValue(ListItemsProperty, value); }
+        }
+
+        private void TextItemList_MouseClick(object sender, MouseButtonEventArgs e)
+        {
+            var wnd = Application.Current.MainWindow as MainWindow;
+            wnd.DocumentsManager.SelectDocument(((TextItemList)sender).TextItem);
+        }
+    }
+}
